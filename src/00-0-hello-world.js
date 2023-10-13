20ee23f44ef0b3bfcc41ea7350f2eaafd604ac14
Keegan Leary <keegan@webbae.net>
2023-10-13 12:40:36 -0700

    PR

--- src/00-0-hello-world.js
+++ src/00-0-hello-world.js
@@ -2,7 +2,6 @@ function greet() {
   // Your Code Here
   // input: none
   // output: returns the string "Hello, World!"
-  return "Hello, World!";
 }
 
 function logWhatsUp() {
@@ -10,5 +9,4 @@ function logWhatsUp() {
   // input: none
   // output: none
   // side effect: logs the string "What's up?" to the console
-  console.log("What's up?");
 }
